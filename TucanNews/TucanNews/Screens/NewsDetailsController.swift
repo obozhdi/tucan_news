93707e6bca24abe1e552509dabc8cd8422081e0c
Artem Pashkov <obozhdi@obozhdi.com>
2021-01-22 19:21:17 +0200

    add project organization

---
+++ TucanNews/TucanNews/Screens/NewsDetailsController.swift
@@ -0,0 +1,8 @@
+//
+//  NewsDetailsController.swift
+//  TucanNews
+//
+//  Created by Artem Pashkov on 22.01.2021.
+//
+
+import Foundation
---
+++ TucanNews/TucanNews/Screens/NewsListController.swift
@@ -0,0 +1,8 @@
+//
+//  NewsListController.swift
+//  TucanNews
+//
+//  Created by Artem Pashkov on 22.01.2021.
+//
+
+import Foundation
