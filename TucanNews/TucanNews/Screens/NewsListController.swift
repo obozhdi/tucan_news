45fa028ab16386da3f2153c699da712a90f1bd1a
Artem Pashkov <obozhdi@obozhdi.com>
2021-01-22 20:04:27 +0200

    add newslist cells layout

--- TucanNews/TucanNews/Screens/NewsListController.swift
+++ TucanNews/TucanNews/Screens/NewsListController.swift
@@ -32,6 +32,7 @@ private extension NewsListController {
       $0.edgesToSuperview()
       $0.delegate = self
       $0.dataSource = self
+      $0.separatorStyle = .none
       
       $0.register(NewsListCell.self, forCellReuseIdentifier: NewsListCell.reuseIdentifier)
     }
@@ -68,27 +69,68 @@ final class NewsListCell: UITableViewCell {
   required init?(coder: NSCoder) { fatalError("init(coder:) has not been implemented") }
   
   private func setupSubviews() {
-    cellImageView.add(to: contentView).do {
-      $0.leftToSuperview(offset: 16)
-      $0.topToSuperview(offset: 8)
-      $0.bottomToSuperview(offset: -8)
-      $0.aspectRatio(1.0)
-      $0.backgroundColor = .lightGray
-    }
+    contentView.backgroundColor = #colorLiteral(red: 0.9803921569, green: 0.9803921569, blue: 0.9803921569, alpha: 1)
+    backgroundColor = #colorLiteral(red: 0.9803921569, green: 0.9803921569, blue: 0.9803921569, alpha: 1)
     
-    cellTitleLabel.add(to: contentView).do {
-      $0.leftToRight(of: cellImageView, offset: 16)
-      $0.centerYToSuperview()
-      $0.rightToSuperview(offset: -16)
+    UIView().add(to: contentView).do {
+      $0.leftToSuperview(offset: 14)
+      $0.rightToSuperview(offset: -14)
+      $0.bottomToSuperview(offset: -14)
+      $0.topToSuperview(offset: 14)
+      $0.backgroundColor = .white
+      $0.layer.shadowColor = UIColor.black.cgColor
+      $0.layer.shadowOpacity = 0.2
+      $0.layer.shadowOffset = .zero
+      $0.layer.shadowRadius = 4
+
+      
+      cellImageView.add(to: $0).do {
+        $0.leftToSuperview()
+        $0.topToSuperview()
+        $0.rightToSuperview()
+        $0.height(190)
+        $0.contentMode = .scaleAspectFill
+        $0.clipsToBounds = true
+      }
+      
+      cellTitleLabel.add(to: $0).do {
+        $0.topToBottom(of: cellImageView, offset: 14)
+        $0.leftToSuperview(offset: 20)
+        $0.rightToSuperview(offset: -20)
+        $0.numberOfLines = 0
+        $0.font = UIFont.systemFont(ofSize: 21, weight: .semibold)
+        $0.textColor = .black
+      }
+      
+      cellDateLabel.add(to: $0).do {
+        $0.topToBottom(of: cellTitleLabel, offset: 8)
+        $0.leftToSuperview(offset: 20)
+        $0.rightToSuperview(offset: -20)
+        $0.numberOfLines = 1
+        $0.font = UIFont.systemFont(ofSize: 14, weight: .regular)
+        $0.textColor = UIColor.black.withAlphaComponent(0.4)
+      }
+      
+      cellTeaserLabel.add(to: $0).do {
+        $0.topToBottom(of: cellDateLabel, offset: 8)
+        $0.leftToSuperview(offset: 20)
+        $0.rightToSuperview(offset: -20)
+        $0.numberOfLines = 0
+        $0.font = UIFont.systemFont(ofSize: 12, weight: .regular)
+        $0.textColor = UIColor.black.withAlphaComponent(0.7)
+        $0.bottomToSuperview(offset: -20)
+      }
     }
   }
   
   func setData(with object: NewsObject) {
     cellImageView.image = object.image
     cellTitleLabel.text = object.title
-    cellDateLabel.text = String(describing: object.date)
+    cellDateLabel.text = String(describing: object.date).uppercased()
     cellTeaserLabel.text = object.teaser
   }
+  
+  override func setSelected(_ selected: Bool, animated: Bool) { selectionStyle = .none }
   
 }
 
