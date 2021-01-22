8d52d44402de15d7d424ea6490e72d74794a13ce
Artem Pashkov <obozhdi@obozhdi.com>
2021-01-22 19:14:04 +0200

    create project

---
+++ TucanNews/TucanNews/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  TucanNews
+//
+//  Created by Artem Pashkov on 22.01.2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    // Do any additional setup after loading the view.
+  }
+
+
+}
+
